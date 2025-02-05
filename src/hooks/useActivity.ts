83adb02f61bf4661017e1237c93068be2824d428
hfelices92 <hfelicesg@fp.insjoaquimmir.cat>
2025-02-05 12:15:03 +0100

    build

---
+++ src/hooks/useActivity.ts
@@ -0,0 +1,13 @@
+import { useContext } from "react"
+import { ActivityContext } from "../context/ActivityContext"
+
+
+export const useActivity = () =>{
+    const context = useContext(ActivityContext)
+
+    if(!context){
+        throw new Error('useActivity must be use within a provider')
+    }
+
+    return context
+}
